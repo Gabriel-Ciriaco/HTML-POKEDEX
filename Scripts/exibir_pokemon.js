2e6b2ae6af4853855531e5a1380d154b710dc1da
YanDutrx <yanmartinsdutra@gmail.com>
2024-12-09 16:09:19 -0300

    modificacoes 9/12
    alinhamento de caixas de texto e cor de fundo.

--- Scripts/exibir_pokemon.js
+++ Scripts/exibir_pokemon.js
@@ -3,6 +3,6 @@ const QTD_POKEMONS = 100;
 for (let i = 0; i < QTD_POKEMONS; i++)
 {
     let pokemon = POKEDEX_JSON[i];
-
+    
     console.log(`${i + 1}. Nome: ${pokemon.nome}, Tipo: ${pokemon.tipo}`);
 }
